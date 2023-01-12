96c08dad0a8c485c9875adca0031239d54ded99f
jholland <jamie_holland04@hotmail.com>
2023-01-12 11:48:24 +0000

    Build scripts.

--- script/build_run.sh
+++ script/build_run.sh
@@ -7,4 +7,4 @@ echo "sqlc-grpc"
 sqlc-grpc -m "my/module/path"
 
 echo "go run"
-go run . -db postgresql://postgres:pass1234@localhost:5432/books -dev -grpcui
+go run . -db postgresql://postgres:pass1234@localhost:5432/authors -dev -grpcui
